fcb084726b25daaac260e08663eeabff5c1efb5b
Cailyn Miller <cailynmiller@Cailyns-Air.lan1>
2021-02-02 19:57:13 -0600

    done with modules, deliverables 1 and 2

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,50 @@
+SELECT emp_no,first_name,last_name FROM employees;
+
+SELECT title,from_date,to_date FROM titles;
+
+-- Creating retirement titles table by joining employees and titles
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t ON (e.emp_no=t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no ASC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, from_date DESC;
+
+-- Retrieve number of titles from unique titles
+SELECT COUNT (emp_no) emp_no,title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY emp_no DESC;
+
+-- Retrieve list of people eligible for mentorship
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de ON (e.emp_no=de.emp_no)
+INNER JOIN titles as t ON (e.emp_no=t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no ASC;
